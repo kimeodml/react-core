eb2e24c03663601a0323e068854496aae2fd2ba8
Daeeui Kim <kimeodml@gmail.com>
2025-01-18 17:15:45 +0900

    refactor: 불필요한 조건문 제거

--- src/utils/createDOM.js
+++ src/utils/createDOM.js
@@ -1,15 +1,14 @@
 export function createDOM(element) {
-  if (typeof element === "string") {
-    return document.createTextNode(element);  // 텍스트 노드를 반환
+  if (typeof element === 'string') {
+    return document.createTextNode(element); // 텍스트 노드를 반환
   }
 
-  const dom = document.createElement(element.type);  // type에 맞는 DOM 요소를 생성
+  const dom = document.createElement(element.type); // type에 맞는 DOM 요소를 생성
 
   // props를 순회하며 요소에 속성 설정 (className 등)
   Object.keys(element.props).forEach(key => {
-    if (key === "className") { // className 처리
-      dom.setAttribute(key, element.props[key]);
-    } else if (key !== "children") { // 그 외 id, data- 등
+    // 추후 data나 input의 value 같은 속성 고려 필요
+    if (key !== 'children') {
       dom[key] = element.props[key];
     }
   });
@@ -17,5 +16,5 @@ export function createDOM(element) {
   // children을 재귀적으로 생성
   element.props.children.map(createDOM).forEach(child => dom.appendChild(child));
 
-  return dom;  // 생성된 DOM 요소 반환
+  return dom; // 생성된 DOM 요소 반환
 }
