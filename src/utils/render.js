724b694846c4d6af9d98b78d77248eea1248f982
Daeeui Kim <kimeodml@gmail.com>
2025-01-31 17:52:57 +0900

    refactor: diff 알고리즘 수정, render 로직 수정

--- src/utils/render.js
+++ src/utils/render.js
@@ -1,14 +1,29 @@
-import { createDOM } from '@/utils/createDOM';
+import createDOM from '@/utils/createDOM';
+import diff from '@/utils/diff';
 
+let oldVDOM = null;
 let globalContainer = null;
-
-export function render(element, container) {
-  globalContainer = container;
-  container.innerHTML = '';
-  const dom = createDOM(element);
-  container.appendChild(dom);
-}
 
 export function getGlobalContainer() {
   return globalContainer;
+}
+
+export function render(element, container) {
+  globalContainer = container;
+  const newVDOM = element;
+
+  if (oldVDOM) {
+    // Diffing 알고리즘을 통해 변경된 부분만 업데이트
+    console.log('container', container);
+    diff(oldVDOM, newVDOM, container);
+  } else {
+    // 처음 렌더링 시 전체 DOM을 생성하여 넣어줌
+    const dom = createDOM(newVDOM);
+    container.appendChild(dom);
+  }
+
+  console.log('old', oldVDOM, 'new', newVDOM);
+
+  // 이전 VDOM을 현재 VDOM으로 업데이트
+  oldVDOM = newVDOM;
 }
